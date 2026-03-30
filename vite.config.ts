ffd33bb0e187589339d0c6e1f3608756d0ed6c45
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-03-30 15:33:25 +0700

    feat: add proxy to server in dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "#": path.resolve(__dirname, "."),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "#": path.resolve(__dirname, "."),
+      },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_PROXY,
+          changeOrigin: true,
+          cookieDomainRewrite: "localhost",
+        },
+      },
+    },
+  };
 });
